f8b99588ebe9cc229af86137c09caaee6adda896
John Foster <john.foster@utsa.edu>
2014-09-05 23:06:42 -0500

    Add custom deploy for Travis CI

--- conf.py
+++ conf.py
@@ -288,7 +288,12 @@ REDIRECTIONS = []
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ['git checkout gh-pages',
+                   'git add -A',
+                   'git commit -m "Travis CI autocommit"',
+                   'git push -fq https://${GH_TOKEN}@github.com/$(TRAVIS_REPO_SLUG).git gh-pages > /dev/null'
+                   'git checkout master'
+                  ]
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
