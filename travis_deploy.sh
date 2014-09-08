b2dfb8dfad3cac9b1dec7f047cc49a42a2065cdd
John Foster <john.foster@utsa.edu>
2014-09-07 21:31:10 -0500

    Changes to travis deploy script

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,12 +1,11 @@
 # Get to the Travis build directory, configure git and clone the repo
-pwd
 cd $HOME
 git config --global user.email "jfoster@austin.utexas.edu"
 git config --global user.name "John Foster"
 
 # Commit and Push the Changes
 cd gh-pages
-cp -Rf $HOME/johntfoster/johntfoster-professional-website/output .
+cp -Rf $HOME/build/johntfoster/johntfoster-professional-website/output/* .
 git add -A
 git commit -m "Travis CI autocommit from travis build $TRAVIS_BUILD_NUMBER"
 git push -fq origin gh-pages > /dev/null
